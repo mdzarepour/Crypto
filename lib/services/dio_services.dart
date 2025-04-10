897fe5fc4277e3a37667a3cd7131b07013e2f568
mdzarepour <mdzarepour@gmail.com>
2025-04-10 23:14:20 +0330

    create cypto model and dio service for fetching data

---
+++ lib/services/dio_services.dart
@@ -0,0 +1,13 @@
+import 'package:crypto_pricing/components/constants/urls.dart';
+import 'package:crypto_pricing/components/models/crypto_model.dart';
+
+class DioServices {
+  Future<List<CryptoModel>> getData() async {
+    Response response = await Dio().get(url);
+    List<CryptoModel> cryptoList =
+        response.data['data']
+            .map<CryptoModel>((e) => CryptoModel.fromJsonObject(e))
+            .toList();
+    return cryptoList;
+  }
+}
